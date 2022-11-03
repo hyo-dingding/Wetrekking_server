7675d10fc52bdb806a07be53f5887a287779ae32
seogoo12 <seogoo12@naver.com>
2022-11-03 22:13:50 +0900

    Feature-CORS#6

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import * as express from 'express';
-import { graphqlUploadExpress } from 'graphql-upload';
-import { ValidationPipe } from '@nestjs/common';
+// import { graphqlUploadExpress } from 'graphql-upload';
+// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cors());
   app.use(express.json());
-  // app.enableCors({
-  //   origin: 'http://localhost:3000/',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(graphqlUploadExpress());
   app.enableCors({
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: [
-      'Access-Control-Allow-Headers',
-      'Authorization',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-    ],
+    origin: ['http://localhost:3000/', 'https://wetrekking.kr'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    origin: ['http://localhost:3000', 'https://wetrekking.kr'],
-    exposedHeaders: ['Authorization', 'Set-Cookie', 'Cookie'],
   });
+  // app.useGlobalPipes(new ValidationPipe());
+  // app.use(graphqlUploadExpress());
+  // app.enableCors({
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  //   allowedHeaders: [
+  //     'Access-Control-Allow-Headers',
+  //     'Authorization',
+  //     'X-Requested-With',
+  //     'Content-Type',
+  //     'Accept',
+  //   ],
+  //   credentials: true,
+  //   origin: ['http://localhost:3000', 'https://wetrekking.kr'],
+  //   exposedHeaders: ['Authorization', 'Set-Cookie', 'Cookie'],
+  // });
   // app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
