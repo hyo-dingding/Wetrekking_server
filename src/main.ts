61ac0fe3aea831c0b96c9e05cd081b8d7c0f6206
seogoo12 <seogoo12@naver.com>
2022-11-03 20:26:38 +0900

    [FEATURE] CORS 기능 추가#3

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,18 @@ async function bootstrap() {
   //     origin: 'http://localhost:3000/',
   //   }),
   // );
-  // app.use(express.json());
+  app.use(express.json());
+  app.use('/graphql', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    // res.send(data);
+  });
   // app.enableCors({
   //   origin: '*',
   // });
-  app.enableCors({
-    origin: ['http://localhost:3000/', 'http://34.64.102.157:3000/graphql'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: ['http://localhost:3000/', 'http://34.64.102.157:3000/graphql'],
+  //   credentials: true,
+  // });
   await app.listen(3000);
 }
 bootstrap();
