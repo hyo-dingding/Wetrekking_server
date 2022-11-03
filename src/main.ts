0baa01d8fd6ab8dfdf2faa852ffaff8ddbcb4c7d
seogoo12 <seogoo12@naver.com>
2022-11-03 22:09:59 +0900

    Feature-CORS#6

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import * as express from 'express';
+import { graphqlUploadExpress } from 'graphql-upload';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(
-    cors({
-      origin: 'http://localhost:3000/',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    }),
-  );
+  app.use(cors());
   app.use(express.json());
+  // app.enableCors({
+  //   origin: 'http://localhost:3000/',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(graphqlUploadExpress());
   app.enableCors({
-    origin: 'http://localhost:3000/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: [
+      'Access-Control-Allow-Headers',
+      'Authorization',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+    ],
     credentials: true,
+    origin: ['http://localhost:3000', 'https://wetrekking.kr'],
+    exposedHeaders: ['Authorization', 'Set-Cookie', 'Cookie'],
   });
+  // app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
 bootstrap();
