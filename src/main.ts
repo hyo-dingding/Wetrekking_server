f55f72e9eac24ea99cbd96d73c5da9be5d657762
Hoon2 <gooaba1204@gmail.com>
2022-11-03 20:57:22 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
-  app.use(express.json());
   app.enableCors({
     origin: ['http://localhost:3000/'],
     credentials: true,
