799d88f80cb721d7485fca7c4cb363e2a10ff2e8
seogoo12 <seogoo12@naver.com>
2022-11-03 22:21:03 +0900

    Feature-CORS#6

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { graphqlUploadExpress } from 'graphql-upload';
 // import { graphqlUploadExpress } from 'graphql-upload';
 // import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cors());
-  app.use(express.json());
+  // const app = await NestFactory.create(AppModule, { cors: true });
+  // app.use(cors());
+  // app.use(express.json());
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000/', 'https://wetrekking.kr'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: 'https://wetrekking.kr',
     credentials: true,
   });
+  app.use(graphqlUploadExpress());
+  // app.useWebSocketAdapter(new SocketIoAdapter(app));
+
+  // app.enableCors({
+  //   origin: ['http://localhost:3000/', 'https://wetrekking.kr'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
   // app.useGlobalPipes(new ValidationPipe());
   // app.use(graphqlUploadExpress());
   // app.enableCors({
