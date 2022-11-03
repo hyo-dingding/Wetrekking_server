8b897bbaa231dc24ee15fbc033178d11e2d283b6
seogoo12 <seogoo12@naver.com>
2022-11-03 15:13:14 +0900

    Feature-CORS#2

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
+import * as cors from 'cors';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const app1 = express();
+  // app1.use(cors());
   app.use(
     cors({
       origin: 'http://localhost:3000/',
     }),
   );
+  app1.use(express.json());
   await app.listen(3000);
 }
 bootstrap();
