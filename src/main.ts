0a6d1ce0a90d72765c02a9c99ea5fa8cefb75860
seogoo12 <seogoo12@naver.com>
2022-11-03 22:38:21 +0900

    Feature-CORS#6

--- src/main.ts
+++ src/main.ts
@@ -8,36 +8,15 @@ async function bootstrap() {
   app.use(cors());
   app.use(express.json());
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // 모든 도메인
-    // res.header("Access-Control-Allow-Origin", "https://example.com"); // 특정 도메인
-  });
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // app.enableCors();
-  // app.use(graphqlUploadExpress());
-  // app.useWebSocketAdapter(new SocketIoAdapter(app));
-
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*'); // 모든 도메인
+  //   // res.header("Access-Control-Allow-Origin", "https://example.com"); // 특정 도메인
+  // });
   app.enableCors({
     origin: ['http://localhost:3000/', 'https://wetrekking.kr'],
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  // app.useGlobalPipes(new ValidationPipe());
-  // app.use(graphqlUploadExpress());
-  // app.enableCors({
-  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  //   allowedHeaders: [
-  //     'Access-Control-Allow-Headers',
-  //     'Authorization',
-  //     'X-Requested-With',
-  //     'Content-Type',
-  //     'Accept',
-  //   ],
-  //   credentials: true,
-  //   origin: ['http://localhost:3000', 'https://wetrekking.kr'],
-  //   exposedHeaders: ['Authorization', 'Set-Cookie', 'Cookie'],
-  // });
-  // app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
 bootstrap();
