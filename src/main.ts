fc1d8fb9503a2fe649fa266298ac90d91f0578e3
seogoo12 <seogoo12@naver.com>
2022-11-05 21:36:19 +0900

    [FIX] CORS 장애#1

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
-import * as express from 'express';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-  app.use(cors());
-  app.use(express.json());
 
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*'); // 모든 도메인
-  //   // res.header("Access-Control-Allow-Origin", "https://example.com"); // 특정 도메인
-  // });
+  app.useGlobalPipes(new ValidationPipe());
+
   app.enableCors({
-    origin: ['http://localhost:3000/', 'https://wetrekking.kr'],
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: ['http://localhost:3000', 'https://develop.wetrekking.kr'],
     credentials: true,
   });
+  app.use(graphqlUploadExpress());
   app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
