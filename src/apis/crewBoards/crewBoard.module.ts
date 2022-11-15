b44b5df916d8d81d60a3d5302c93923df22a7c83
hyo-dingding <zvoniti2964@gmail.com>
2022-11-15 16:31:04 +0900

    [FEATURE] crewBoardModule dib 추가

--- src/apis/crewBoards/crewBoard.module.ts
+++ src/apis/crewBoards/crewBoard.module.ts
@@ -3,6 +3,8 @@ import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CrewBoardImageService } from '../crewBoardImages/crewBoardImage.service';
 import { CrewBoardImage } from '../crewBoardImages/entities/crewBoardImage.entity';
+import { DibService } from '../dib/dib.service';
+import { Dib } from '../dib/entities/dib.entity';
 import { Mountain } from '../mountains/entities/mountain.entity';
 import { User } from '../users/entities/user.entity';
 import { CrewBoardResolver } from './crewBoard.resolver';
@@ -16,7 +18,7 @@ import { CrewBoard } from './entities/crewBoard.entity';
       CrewBoardImage,
       User,
       Mountain,
-
+      Dib,
     ]),
     ElasticsearchModule.register({
       node: 'http://elasticsearch:9200',
