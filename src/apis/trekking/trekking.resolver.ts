e65dda4cac0def335fa1621fec6b85082b83d6b2
hoon2-kim <gooaba1204@gmail.com>
2022-11-17 21:38:54 +0900

    [MODIFY] trekking API 수정본

--- src/apis/trekking/trekking.resolver.ts
+++ src/apis/trekking/trekking.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Trekking } from './schemas/trekking.schema';
+import { TrekkingInfo } from './schemas/trekkingInfo.schema';
 import { TrekkingService } from './trekking.service';
 
 @Resolver()
@@ -8,17 +9,27 @@ export class TrekkingResolver {
     private readonly trekkingService: TrekkingService, //
   ) {}
 
-  @Query(() => Trekking)
-  async fetchTrekkingInfo(
-    @Args('address') address: string, //
-    @Args('mountainName') mountainName: string,
-  ) {
-    const qqq = await this.trekkingService.getEmdCdInfo({ address });
+  // @Query(() => Trekking)
+  // async fetchTrekkingInfo(
+  //   @Args('address') address: string, //
+  //   @Args('mountainName') mountainName: string,
+  // ) {
+  //   const qqq = await this.trekkingService.getEmdCdInfo({ address });
 
-    return this.trekkingService.getTrekkingInfo({ qqq, mountainName });
+  //   return this.trekkingService.getTrekkingInfo({ qqq, mountainName });
+  // }
+
+  @Query(() => [TrekkingInfo])
+  async fetchTrekkingCoordinate(
+    @Args('mountainName') mountainName: string, //
+  ) {
+    return this.trekkingService.findTrekking({ mountainName });
   }
 
-  @Mutation(() => String)
+  @Mutation(() => [TrekkingInfo], {
+    description:
+      'MongoDB에 산+등산로 좌표를 저장하기 위한 API, 누르지 마요 큰일나요',
+  })
   async createMongoDBTrekking() {
     return this.trekkingService.saveTrekking();
   }
